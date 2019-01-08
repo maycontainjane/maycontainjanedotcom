f3bfbfc7a998592deb1f1abe7ac004f4b07532e2
Jane Kagan <jkagan@cray.com>
2019-01-08 14:55:10 -0600

    Mobile-ready home page! Moving right along

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,22 +8,22 @@ const HomePage = () => (
         <Header title="Welcome!" subtitle={<div>Hi, I'm <span>Jane</span>. I write code.</div>} />
         <div className="homepage__body">
             <div className="homepage__body-img">
-                <img width="250px" src="/img/chance.JPG" />
+                <img src="/img/chance.JPG" />
             </div>
             <div className="homepage__body-text">
                 <p className="homepage__body-p">I’m an engineer living in Minneapolis, Minnesota. I love solving 
                 problems with software and making the web ecosystem a more beautiful place.</p> 
                 <h3>I'm best at:</h3>
                 <div className="homepage__body-lists">
-                    <ul className="homepage__body-ol">
+                    <ul className="homepage__body-ul">
                         <li>Integration</li>
                         <li>DevOps</li>
                     </ul>
-                    <ul className="homepage__body-ol">
+                    <ul className="homepage__body-ul">
                         <li>Testing</li>
                         <li>Web development</li>
                     </ul>
-                    <ul className="homepage__body-ol">
+                    <ul className="homepage__body-ul">
                         <li>Python</li>
                         <li>Linux</li>
                     </ul>
